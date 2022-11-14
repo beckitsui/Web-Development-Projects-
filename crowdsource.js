5b1a843303feb383723f161743240e1f5ee08385
beckitsui <107963823+beckitsui@users.noreply.github.com>
2022-11-14 10:34:58 -0500

    right now everything is working excel for the [response]; I am adding a submit button; hope I do not mess u

--- crowdsource.js
+++ crowdsource.js
@@ -41,7 +41,7 @@ class Calculator{
   //create method
   _select(itemElement){
     const index = Array.from(this.listConcern.children).indexOf(itemElement);
-    console.log(index);
+    
     this.originalSelect.querySelectorAll('option')[index].selected = false;
 
     itemElement.classList.remove("select__item--selected");
@@ -70,45 +70,45 @@ for (let c = 0; c < all_concerns.length; c++){
 // const ulEl = document.querySelector(".calculator");
 
 
-// //1.create calculator items
+//1.create calculator items
 
 
-// //2.parse the function
-// populateItems(listConcerns1);
-// populateItems(listConcerns2);
-// populateItems(listConcerns3);
-// populateItems(listConcerns4);
-// populateItems(listConcerns5);
+//2.parse the function
+populateItems(listConcerns1);
+populateItems(listConcerns2);
+populateItems(listConcerns3);
+populateItems(listConcerns4);
+populateItems(listConcerns5);
 
-// //3.minor styling
-// const selectedColor = "salmon"
+//3.minor styling
+const selectedColor = "salmon"
 
-// //functions
-// function populateItems(InfoList) {
-//   let listItems = "";
-//   for (let i = 0; i < InfoList.length; i++) {
-//     listItems += `<li class='calItem'> ${InfoList[i]} </li>`
-//   }
-//   ulEl.innerHTML += `<div class='calSubGroup'> Title(to work on changing line) ${listItems} </li>`;
-// }
+//functions
+function populateItems(InfoList) {
+  let listItems = "";
+  for (let i = 0; i < InfoList.length; i++) {
+    listItems += `<li class='calItem'> ${InfoList[i]} </li>`
+  }
+  ulEl.innerHTML += `<div class='calSubGroup'> Title(to work on changing line) ${listItems} </li>`;
+}
 
-// let liEl = document.getElementsByClassName("calItem")
-// console.log(liEl)
+let liEl = document.getElementsByClassName("calItem")
+console.log(liEl)
 
-// let userPreference = []
-// for (let i=0; i<liEl.length; i++){
-//   liEl[i].value = 0
-// //DEBUG: need to select each item that has a class calItem
-//   liEl[i].addEventListener("click", function(){
-//     if (liEl[i].value === 0){
-//       liEl[i].classList.add("selectedButton");
-//       liEl[i].value = 1
-//       userPreference.push(liEl[i].textContent);
-//     }
-//     else{
-//       liEl[i].classList.remove("selectedButton")
-//       liEl[i].value = 0
-//       userPreference.splice(i,1)
-//     }
-//   })
-// }
+let userPreference = []
+for (let i=0; i<liEl.length; i++){
+  liEl[i].value = 0
+//DEBUG: need to select each item that has a class calItem
+  liEl[i].addEventListener("click", function(){
+    if (liEl[i].value === 0){
+      liEl[i].classList.add("selectedButton");
+      liEl[i].value = 1
+      userPreference.push(liEl[i].textContent);
+    }
+    else{
+      liEl[i].classList.remove("selectedButton")
+      liEl[i].value = 0
+      userPreference.splice(i,1)
+    }
+  })
+}
--- crowdsource3-Becky.js
+++ crowdsource3-Becky.js
@@ -1,68 +1,76 @@
-const containter = document.querySelector("#calculator-contain");
+const container = document.querySelector("#big_calculator_container");
+const title = document.createElement("h1")
+title.innerHTML = "This is our lovely calculator"
+container.append(title)
 
 const calculatorQuestions = [
   {
-    title: "What is your address",
-    numberOfOptions: 5,
-    questionType: "multiple",
-    response: 2,
-    answers:{
-        1: 500,
-        2: 300,
-        3: 400,
-        4: 400,
-        5: 100,
-    }
+    title: "What is your householdsize?",
+    numberOfOptions: 8,
+    listOfOptions: [1,2,3,4,5,6,7,8],
+    questionType: "single",
+    response: [],
+    // answers:{
+    //     1: 500,
+    //     2: 300,
+    //     3: 400,
+    //     4: 400,
+    //     5: 100,
+    // }
   },
   {
+    //do we wanna do this as a slider
     title: "What is your income",
-    numberOfOptions: 5,
+    numberOfOptions: 8,
+    listOfOptions: ["10000-20000", "20000-30000", "30000-40000", "40000-50000", "50k-60k"],
     questionType: "single",
     response: [],
   },
   {
     title: "how old is your house",
     numberOfOptions: 5,
+    listOfOptions: ["<1 year", "2-10years", "20-50years", "freaking old crippling"],
     questionType: "single",
     response: [],
   },
   {
-    title: "how tall are you",
+    title: "What are your concerns?",
     numberOfOptions: 5,
+    listOfOptions: ['energy bills', 'summer heat', 'solar opportunities', 'others'],
     questionType: "multiple",
     response: [],
   },
   {
     title: "what is your favourite fruit",
     numberOfOptions: 5,
+    listOfOptions: ['apple', 'orange', 'grapes', 'banana', 'kiwi'],
     questionType: "multiple",
     response: [],
   },
 ];
 
 calculatorQuestions.forEach((el, index) => {
-  // create original container that is the parent to specific question
-  const questionContainer = document.createElement("div");
-  questionContainer.style.backgroundColor = "red";
+  // Grand Parent: create original container that is the parent to specific question
+  const calculatorContainer = document.createElement("div");
+  calculatorContainer.classList.add("calculator_question_container");
 
-  //   create the tile
+  // Parent A:create the tile
   const questionTitle = document.createElement("h3");
   questionTitle.innerHTML = el.title;
 
-  /**
+  /** this is called doc string
    * Add all of the other elements here below
    */
 
-  //   create parent container of choices
+  // Parent B - Parent: create parent container of choices
   const choicesContainer = document.createElement("div");
-  choicesContainer.style.display = "grid";
-  choicesContainer.style.gridTemplateColumns = "repeat(6, 1fr)";
-  choicesContainer.style.gap = "1rem";
-
+  choicesContainer.classList.add("calculator_choices_container");
+  
+  // Childrens of Parent B - iterative
   for (let i = 0; i < el.numberOfOptions; i++) {
     const choices = document.createElement("button");
-    choices.style.backgroundColor = "blue";
-    choices.innerHTML = "Hi";
+    choices.classList.add("choices_normal");
+    choices.innerHTML = el.listOfOptions[i];
 
     // create a function to handle the click
 
@@ -74,7 +82,7 @@ calculatorQuestions.forEach((el, index) => {
        *
        * if not a muilti answer, replace the existing value with this value
        */
-      const proposedValue = i + 1;
+      const proposedValue = i;
 
       if (el.questionType === "multiple") {
         if (el.response.includes(proposedValue)) {
@@ -86,7 +94,7 @@ calculatorQuestions.forEach((el, index) => {
       } else {
         el.response = [proposedValue];
       }
-
+////////////figure it out becky!!!!!
       document.getElementById(`${index}-answer`).innerHTML =
         el.response.join(" ");
     }
@@ -101,9 +109,9 @@ calculatorQuestions.forEach((el, index) => {
   choicesContainer.append(pickedAnswer);
 
   //   append all objects to the parent container
-  questionContainer.append(questionTitle);
-  questionContainer.append(choicesContainer);
+  calculatorContainer.append(questionTitle);
+  calculatorContainer.append(choicesContainer);
 
   //   append the parent container of this question to the existing dom object "#calculator-contain"
-  containter.append(questionContainer);
+  container.append(calculatorContainer);
 });
